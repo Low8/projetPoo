55f22633ce73f41c3a4dfde9dd323f91edd4a3d1
Low8 <louison.masson@gmail.com>
2024-11-27 15:31:56 +0000

    V1 grille

--- Grille.h
+++ Grille.h
@@ -4,16 +4,28 @@ class Grille
 {
 private:
     vector<vector<int>> table;
-    
+    int nbLigne;
+    int nbColonne;
 public:
-    Grille(/* args */);
+    Grille(int ligne, int colonne);
     ~Grille();
+    void affiche();
 };
 
-Grille::Grille(/* args */)
+Grille::Grille(int ligne, int colonne) : nbLigne(ligne), nbColonne(colonne)
 {
+    table = vector<vector<int>>(nbLigne, vector<int>(nbColonne, 0));
 }
 
 Grille::~Grille()
 {
+}
+
+void Grille::affiche() {
+    for (int i = 0; i<nbColonne; i++){
+        for(int j = 0; j<nbLigne; j++){
+            cout<<table[i][j]<< "\t";
+        }
+        std::cout << endl;
+    }
 }
