149d6a621504e4491a4c8d86aee2f9681cbc0bae
Elodie Simon <elodie.simon@iacesi.fr>
2024-11-28 10:37:04 +0100

    classe règles v1

--- Regle.h
+++ Regle.h
@@ -1,51 +1,52 @@
+#include "Librairie.h"
+#include "Cellule.h"
 
-
-int adjacent(int i, int j,const vector<vector<int>>&vecteurs) {
-    int compt = 0;
-    for (int k = i - 1; k<=i+1; k++){
-        for(int q = j-1; q <= j+1; q++){
-            if(k>=0 && k< 10 && q>= 0 && q<10 && !(k==i && q == j)){
-                if(vecteurs[k][q] == 1){
-                    compt++;
+class Regle{
+public:
+    int adjacent(int i, int j,const vector<vector<Cellule>>&vecteurs) {
+        int compt = 0;
+        for (int k = i - 1; k<=i+1; k++){
+            for(int q = j-1; q <= j+1; q++){
+                if(k>=0 && k< 10 && q>= 0 && q<10 && !(k==i && q == j)){
+                    if(vecteurs[k][q].estVivant()){
+                        compt++;
+                    }
                 }
             }
         }
+        return compt;
     }
-    return compt;
-}
-
-
-void genarationSuiv(vector<vector<int>>&vecteurs) {
-    vector<vector<int>>temp(10, vector<int>(10,0));
-    for (int i = 0; i < 10; i++)
-    {
-            for (int j = 0; j < 10; j++)
-        {
-            if (vecteurs[i][j] == 1)
-            {
-                if (adjacent(i, j, vecteurs) == 2 || adjacent(i, j, vecteurs) == 3)
-                {
-                    temp[i][j] = 1;
-                } else {
-                    temp[i][j] = 0;
+    
+    
+    void genarationSuiv(vector<vector<Cellule>>&vecteurs) {
+        vector<vector<Cellule>>temp(10, vector<Cellule>(10));
+        for (int i = 0; i < 10; i++){
+                for (int j = 0; j < 10; j++){
+                    if (vecteurs[i][j].estVivant()){
+                        if (adjacent(i, j, vecteurs) == 2 || adjacent(i, j, vecteurs) == 3){
+                            temp[i][j].SetEtat(true);
+                        } else {
+                            temp[i][j].SetEtat(false);
+                        }
+                    }else{
+                        if (adjacent(i, j, vecteurs) == 3) {
+                            temp[i][j].SetEtat(true);
+                        } else{
+                            temp[i][j].SetEtat(false);                            
+                        }
+                    }
                 }
-            } else
-            {
-                if (adjacent(i, j, vecteurs) == 3)
-                {
-                    temp[i][j] = 1;
-                } else
-                {
-                    temp[i][j] = 0;
-                }
+        }
+            vecteurs = temp;
+    }
+    
+    void affiche(vector<vector<Cellule>>&vecteurs){
+        for (int i = 0; i<=10; i++){
+            for (int j = 0; j<=10; j++){
+                cout<<(vecteurs.estVivant() ? 1 : 0) << "\t" ;
             }
+            cout<<endl;
         }
-    }
-    for (int i = 0; i < 10; i++)
-    {
-        for (int j = 0; j < 10; j++)
-        {
-            vecteurs[i][j] = temp[i][j];
         }
-    }
-}
+};
+
